9cbaca31b046975d52bbddd542a637ef8864c012
n-n06 <nursultanzhantuar@gmail.com>
2025-03-30 17:06:25 +0500

    [Add] logout function, auth interceptor

---
+++ frontend/music-app/src/app/auth/auth.interceptor.ts
@@ -0,0 +1,60 @@
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { AuthService } from "./auth.service";
+import { inject } from "@angular/core";
+import { catchError, Observable, switchMap, throwError } from "rxjs";
+
+let isRefreshing = false;
+
+export const authTokenInterceptorFn : HttpInterceptorFn = (
+    req : HttpRequest<any>, 
+    next : HttpHandlerFn
+) => {
+  const authService : AuthService = inject(AuthService);
+  const accessToken = authService.accessToken;
+
+  if (!accessToken) {
+    console.log('nema');
+  }
+
+  if (isRefreshing) {
+    return refreshTokenInterceptor(authService, req, next);
+  }
+
+  return next(addTokenToHeaders(req, accessToken)).pipe(
+    catchError(err => {
+      if (err.status === 403) {
+        return refreshTokenInterceptor(authService, req, next);
+      }
+
+      return throwError(() => err);
+    })
+  );
+}
+
+const refreshTokenInterceptor = (
+  authService : AuthService, 
+  req : HttpRequest<any>,   
+  next : HttpHandlerFn
+) => {
+
+  if (!isRefreshing) {
+    isRefreshing = true;
+    return authService.refreshAuthToken()
+    .pipe(
+      switchMap((res) => {
+        isRefreshing = false;
+        return next(addTokenToHeaders(req, res.access));
+      })
+    );
+  }
+
+  return next(addTokenToHeaders(req, authService.accessToken));
+}
+
+const addTokenToHeaders = (req : HttpRequest<any>, token : string | null) => {
+  return req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+}
--- frontend/music-app/src/app/auth/auth.service.ts
+++ frontend/music-app/src/app/auth/auth.service.ts
@@ -1,19 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 import { TokenResponse, UserLogin, UserRegistration } from './auth.interface';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  http: HttpClient = inject(HttpClient);
+  http : HttpClient = inject(HttpClient);
   router : Router = inject(Router);
-  apiUrl = 'localhost:0000/auth/'
+  cookie : CookieService = inject(CookieService);
 
-  token : string = '';
-  refreshToken : string = ''; //it should redirect us back to the login page after rega
+  apiUrl : string = 'localhost:0000/auth/'
+  accessToken : string | null = '';
+  refreshToken : string | null= ''; //it should redirect us back to the login page after rega
 
   registered : boolean = false;
 
@@ -22,26 +24,57 @@ export class AuthService {
   }
 
   isAuthenticated() : boolean {
-    return false; // TODO: FIX when auth works
-  }
-
-  login(payload: UserLogin) {
-
-    return this.http.post<TokenResponse>(this.apiUrl, payload)
-      .pipe(
-        tap(res => {
-          this.token = res.access;
-          this.refreshToken = res.refresh;
-        })
-      );
+    if (!this.accessToken) {
+      this.accessToken = this.cookie.get('accessToken');
+      this.refreshToken = this.cookie.get('refreshToken');
+    }
+    return !!this.accessToken; 
   }
 
   register(payload: UserRegistration) {
-    
-    return this.http.post(this.apiUrl, payload).pipe(
+    return this.http.post(this.apiUrl, payload).pipe( //TODO:  add actual url later
       tap(
         () => this.registered = true
       )
     );
   }
+
+  login(payload: UserLogin) {
+    return this.http.post<TokenResponse>(this.apiUrl, payload) //TODO: add actual url later
+      .pipe(
+        tap(res => {
+          this.saveTokens(res);
+        })
+      );
+  }
+
+  refreshAuthToken() {
+    return this.http.post<TokenResponse>(`${this.apiUrl}refresh`, { //TODO: add actual url later
+      refresh_token: this.refreshToken
+    }).pipe(
+      tap(
+        res => this.saveTokens(res)
+      ),
+      catchError(err => {
+        this.logout();
+        return throwError(() => err);
+      })
+    )
+  }
+
+  logout() {
+    this.cookie.deleteAll();
+    this.accessToken = null;
+    this.refreshToken = null;
+    this.router.navigate(['/login']);
+  }
+
+  saveTokens(res: TokenResponse) {
+    this.accessToken = res.access;
+    this.refreshToken = res.refresh;
+
+    this.cookie.set('accessToken', this.accessToken);
+    this.cookie.set('refreshToken', this.refreshToken);
+  }
+
 }
