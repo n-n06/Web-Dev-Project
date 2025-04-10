0e371fa3f6faaff360261deae3f8783fd5dce6d4
n-n06 <nursultanzhantuar@gmail.com>
2025-04-10 19:27:56 +0500

    [Major Fix] switch to postgresql

--- backend/web_dev_back/settings.py
+++ backend/web_dev_back/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +50,6 @@ INSTALLED_APPS = [
     'music.apps.MusicConfig',
     'rest_framework',
     'rest_framework.authtoken',
-
 ]
 
 REST_FRAMEWORK = {
@@ -88,9 +97,14 @@ WSGI_APPLICATION = 'web_dev_back.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
